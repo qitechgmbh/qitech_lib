ff147c8912cc9559a9914b19a2dae4e6f4d86636
Hans Sebastian Pöhlmann <contact@hsp.dev>
2026-05-22 16:54:15 +0200

    added 750_553 driver and 553+551 registration

--- ethercat_hal/src/devices/wago_750_354.rs
+++ ethercat_hal/src/devices/wago_750_354.rs
@@ -18,6 +18,8 @@ use crate::devices::{
         wago_750_455::{WAGO_750_455_MODULE_IDENT, WAGO_750_455_PRODUCT_ID},
         wago_750_501::{WAGO_750_501_MODULE_IDENT, WAGO_750_501_PRODUCT_ID},
         wago_750_530::{WAGO_750_530_MODULE_IDENT, WAGO_750_530_PRODUCT_ID},
+        wago_750_531::{WAGO_750_531_MODULE_IDENT, WAGO_750_531_PRODUCT_ID},
+        wago_750_553::{WAGO_750_553_MODULE_IDENT, WAGO_750_553_PRODUCT_ID},
         wago_750_652::{WAGO_750_652_MODULE_IDENT, WAGO_750_652_PRODUCT_ID},
         wago_750_1506::{WAGO_750_1506_MODULE_IDENT, WAGO_750_1506_PRODUCT_ID},
     },
@@ -288,6 +290,16 @@ impl Wago750_354 {
                     module.has_rx = true;
                     module.name = "750-530".to_string();
                 }
+                WAGO_750_531_PRODUCT_ID => {
+                    module.has_tx = false;
+                    module.has_rx = true;
+                    module.name = "750-531".to_string();
+                }
+                WAGO_750_553_PRODUCT_ID => {
+                    module.has_tx = true;
+                    module.has_rx = true;
+                    module.name = "750-553".to_string();
+                }
                 WAGO_750_652_PRODUCT_ID => {
                     module.has_tx = true;
                     module.has_rx = true;
@@ -375,6 +387,8 @@ impl Wago750_354 {
                         WAGO_750_455_MODULE_IDENT => Box::new(wago_750_455::Wago750_455::new()),
                         WAGO_750_501_MODULE_IDENT => Box::new(wago_750_501::Wago750_501::new()),
                         WAGO_750_530_MODULE_IDENT => Box::new(wago_750_530::Wago750_530::new()),
+                        WAGO_750_531_MODULE_IDENT => Box::new(wago_750_531::Wago750_531::new()),
+                        WAGO_750_553_MODULE_IDENT => Box::new(wago_750_553::Wago750_553::new()),
                         WAGO_750_1506_MODULE_IDENT => Box::new(wago_750_1506::Wago750_1506::new()),
                         WAGO_750_652_MODULE_IDENT => Box::new(wago_750_652::Wago750_652::new()),
                         WAGO_750_402_MODULE_IDENT => Box::new(wago_750_402::Wago750_402::new()),
