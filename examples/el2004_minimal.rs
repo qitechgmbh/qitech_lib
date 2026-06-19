41b11a5e0efcefdcbfc8ec551f4f8c5ea5f7c25f
Oshgnacknak <osh@oshgnacknak.de>
2026-06-19 16:36:12 +0200

    Make formatter happy

--- examples/el2004_minimal.rs
+++ examples/el2004_minimal.rs
@@ -1,15 +1,17 @@
 use bitvec::{order::Lsb0, slice::BitSlice};
 use ethercat_hal::{
-    BECKHOFF_VENDOR_ID, EtherCATState, devices::{
+    BECKHOFF_VENDOR_ID, EtherCATState,
+    devices::{
         EthercatDevice, NewEthercatDevice,
         el2004::{EL2004, EL2004_PRODUCT_ID},
-    }, init_ethercat, io::digital_output::DigitalOutputDevice
+    },
+    init_ethercat,
+    io::digital_output::DigitalOutputDevice,
 };
 use std::{env, time::Duration};
 
 /// This example showcases a very bare bones application to toggle the LEDs on an EL2004
 fn main() {
-
     // Initialize EtherCAT Master with the default configuration
     let interface = env::args().nth(1).expect("No Interface-name given");
     let mut eth_control = init_ethercat(&interface, None);
@@ -38,7 +40,10 @@ fn main() {
         eth_control.controller.subdevice_count
     );
     for i in 0..eth_control.controller.subdevice_count {
-        println!(" - {:?}", eth_control.controller.subdevices[i].get_name().unwrap());
+        println!(
+            " - {:?}",
+            eth_control.controller.subdevices[i].get_name().unwrap()
+        );
     }
 
     eth_control
@@ -58,7 +63,6 @@ fn main() {
     let mut el2004: EL2004 = EL2004::new();
 
     for iter in 0.. {
-
         // Tick our application.
         // Here, we just alternate which LED is on
         for i in 0..el2004.get_port_count() {
@@ -68,15 +72,17 @@ fn main() {
         // We ONLY have outputs so no need to call get_inputs
         if let Some(outputs) = eth_control.app_handle.write_outputs() {
             for subdevice in eth_control.controller.get_subdevices() {
-
                 // Loop over the subdevices until the EL2004 is found
-                if subdevice.vendor == BECKHOFF_VENDOR_ID && subdevice.product_id == EL2004_PRODUCT_ID {
-
+                if subdevice.vendor == BECKHOFF_VENDOR_ID
+                    && subdevice.product_id == EL2004_PRODUCT_ID
+                {
                     // Get the part of the Rx PDO that is used by the EL2004
                     let subdevice_outputs = &mut outputs[subdevice.start_rx..subdevice.end_rx];
 
                     // Create the actual Rx PDO and put it into the output
-                    el2004.output(BitSlice::<u8, Lsb0>::from_slice_mut(subdevice_outputs)).expect("Failed to write Rx PDO");
+                    el2004
+                        .output(BitSlice::<u8, Lsb0>::from_slice_mut(subdevice_outputs))
+                        .expect("Failed to write Rx PDO");
                 }
             }
         }
